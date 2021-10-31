8d25412cf073af69a106c3c4b2ae5245971de7f3
AMartins87 <aneta.martins@outlook.com>
2021-10-31 13:42:26 +0000

    Add import random, function get_random_word() and py_play(words) with intro text

--- run.py
+++ run.py
@@ -2,6 +2,45 @@
 Hangman style game with Python
 themed words to be guessed
 """
+import random
 from py_words import words
 
-print(words)
+def get_random_word(words):
+    """
+    This function picks a random word from
+    py_words file
+    """
+    play_word = random.choice(words)
+    return play_word
+print(get_random_word(words))
+
+
+def py_play(words):
+    """
+    This functions starts the application
+    and gives a little intro text about the
+    game and how to play.
+    """
+    print("\n")
+    print("Welcome to the PY MAN game!")
+    print("You may have guessed this is a Python themed game (^_^)")
+    print("Rules are same as in the traditional Hangman.")
+    print("You will type in letters which you think are part of the word.")
+    print("You will have 6 attempts to try and guess the correct word.")
+    print("\n")
+    print("                        .::::::::::.")
+    print("                       .::``::::::::::.")
+    print("                       :::..:::::::::::")
+    print("                       ````````::::::::")
+    print("               .::::::::::::::::::::::: iiiiiii,")
+    print("            .:::::::::::::::::::::::::: iiiiiiiii.")
+    print("            ::::::::::::::::::::::::::: iiiiiiiiii")
+    print("            ::::::::::::::::::::::::::: iiiiiiiiii")
+    print("            :::::::::: ,,,,,,,,,,,,,,,,,iiiiiiiiii")
+    print("            `::::::::: iiiiiiiiiiiiiiiiiiiiiiiiii`")
+    print("               `:::::: iiiiiiiiiiiiiiiiiiiiiii`")
+    print("                       iiiiiiii,,,,,,,,")
+    print("                       iiiiiiiiiii''iii")
+    print("                       `iiiiiiiiii..ii`")
+    print("                         `iiiiiiiiii`")
+    print("\n")
